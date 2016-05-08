433288b8d15fa0c2c3058ec1965afcf3a1d79e20
Naman Aggarwal <naman.aggarwal@yahoo.com>
2016-05-08 18:17:15 +0800

    Skipped test

--- tests/test_insights.py
+++ tests/test_insights.py
@@ -12,6 +12,7 @@ class InsightsTest(unittest.TestCase):
         auth = OAuthAuthentication("gVaoFbo86jmTfOB4NUyGKaAchVEU8ZVPalHQRLTxeaf750b6!414b543630362f426b4f6636415a5973656c33735661383d", "./prod_key.p12", "alias", "password")
         Config.setAuthentication(auth)
 
+    @unittest.skip("Check Key")
     def test_Example_Insights(self):
 
         mapObj = BaseMap()
