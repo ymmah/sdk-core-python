c791b2da873de334e5d67b2c3afd5c6cb0f4a854
Andrea Rizzini <andrea.rizzini@mastercard.com>
2017-12-06 09:09:01 +0000

    arizzini: bumping up version of supported language.

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(name='mastercard_api_core',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',	
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
         tests_require=['mock','nose','coverage','urllib3[secure]'],
