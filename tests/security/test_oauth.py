deaa5fbe04260a9d4c744f354a44b130bcd7ef4b
Andrea Rizzini <andrea.rizzini@mastercard.com>
2017-12-12 15:24:55 +0000

    arizzini: fixing the last issue with the sdk-core

--- tests/security/test_oauth.py
+++ tests/security/test_oauth.py
@@ -56,7 +56,7 @@ class OAuthTest(unittest.TestCase):
     def test_getBodyHash(self):
         body = '<?xml version="1.0" encoding="Windows-1252"?><ns2:TerminationInquiryRequest xmlns:ns2="http://mastercard.com/termination"><AcquirerId>1996</AcquirerId><TransactionReferenceNumber>1</TransactionReferenceNumber><Merchant><Name>TEST</Name><DoingBusinessAsName>TEST</DoingBusinessAsName><PhoneNumber>5555555555</PhoneNumber><NationalTaxId>1234567890</NationalTaxId><Address><Line1>5555 Test Lane</Line1><City>TEST</City><CountrySubdivision>XX</CountrySubdivision><PostalCode>12345</PostalCode><Country>USA</Country></Address><Principal><FirstName>John</FirstName><LastName>Smith</LastName><NationalId>1234567890</NationalId><PhoneNumber>5555555555</PhoneNumber><Address><Line1>5555 Test Lane</Line1><City>TEST</City><CountrySubdivision>XX</CountrySubdivision><PostalCode>12345</PostalCode><Country>USA</Country></Address><DriversLicense><Number>1234567890</Number><CountrySubdivision>XX</CountrySubdivision></DriversLicense></Principal></Merchant></ns2:TerminationInquiryRequest>'
         encodedHash = Util.base64Encode(Util.sha1Encode(body))
-        self.assertEqual(encodedHash,b"WhqqH+TU95VgZMItpdq78BWb4cE=")
+        self.assertEqual( encodedHash, str("WhqqH+TU95VgZMItpdq78BWb4cE="))
 
 
     def test_getBaseString(self):
